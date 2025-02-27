cfd42459399879bb591e471002e2419084e8a6e7
HBehery <hamzahbehery@gmail.com>
2025-02-27 18:31:57 -0500

    making placeholder pages

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center my-64 h-full font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-3xl font-bold">Welcome</h1>
-      <p className="text-lg">Main Page</p>
+      <p className="text-lg">Challenges Page</p>
     </div>
   );
 }
