c1f59f1e9c99863daf11b5f41f07b430ae5a7e1f
HBehery <hamzahbehery@gmail.com>
2025-02-28 11:18:47 -0500

    fixed deployment issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center text-center mx-12 h-[90vh] font-[family-name:var(--font-geist-sans)] bg-no-repeat">
       <h1 className="text-3xl font-bold">
-        Welcome to ICCO's Annual Ramadan Competition.
+        Welcome to ICCO&apos;s Annual Ramadan Competition.
       </h1>
       <p className="text-lg">
         Please proceed to the{" "}
