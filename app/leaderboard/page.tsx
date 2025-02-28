aecebb9846f107abfc2805f05c3f3fbb9c708511
HBehery <hamzahbehery@gmail.com>
2025-02-28 14:01:21 -0500

    leaderboard placeholder update

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+export default function Leaderboard() {
   return (
-    <div className="flex flex-col items-center justify-center my-64 h-full font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl font-bold">Welcome</h1>
-      <p className="text-lg">Leaderboard Page</p>
+    <div className="flex flex-col items-center justify-center text-center mx-12 h-[90vh] font-[family-name:var(--font-geist-sans)] bg-no-repeat">
+      <h1 className="text-3xl font-bold">Leaderboard</h1>
+      <p className="text-lg">
+        Releasing on March 10th 2025 / Ramadan 11th 1446
+      </p>
     </div>
   );
 }
