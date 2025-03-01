9c540cd3ad95340dc26cd8ae220c85b4df71200f
HBehery <hamzahbehery@gmail.com>
2025-03-01 13:45:31 -0500

    frontend tweak

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex flex-col no-doc-scroll items-center justify-center text-center h-[90vh] gap-16 md:gap-24 font-[family-name:var(--font-geist-sans)] bg-no-repeat">
-      <div className="flex justify-center z-[-1] w-[75%] sm:w-[50%]">
+    <div className="flex flex-col no-doc-scroll items-center justify-center text-center h-[90vh] gap-4 font-[family-name:var(--font-geist-sans)] bg-no-repeat">
+      <div className="flex justify-center z-[-1] w-[65%] sm:w-[50%]">
         <img src="/title.png" alt="Title" />
       </div>
       <div className="flex flex-col items-center justify-center px-12 sm:px-20">
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-lg sm:text-2xl font-bold">
           Welcome to ICCO&apos;s Annual Ramadan Competition.
         </h1>
-        <p className="text-lg">
+        <p className="text-md sm:text-lg">
           Please proceed to the{" "}
           <a className="underline " href="/quizzes">
             quizzes
